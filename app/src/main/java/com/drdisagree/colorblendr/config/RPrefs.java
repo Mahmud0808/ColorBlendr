9c70404a1f729659f6f1c29861e6bf9eb454944b
DrDisagree <crazymahmud08@gmail.com>
2023-12-13 16:50:37 +0600

    Add theme backup and restore method

--- app/src/main/java/com/drdisagree/colorblendr/config/RPrefs.java
+++ app/src/main/java/com/drdisagree/colorblendr/config/RPrefs.java
@@ -3,12 +3,24 @@ package com.drdisagree.colorblendr.config;
 import static android.content.Context.MODE_PRIVATE;
 
 import android.content.SharedPreferences;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 
 import com.drdisagree.colorblendr.ColorBlendr;
 import com.drdisagree.colorblendr.common.Const;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.Map;
+
 @SuppressWarnings("unused")
 public class RPrefs {
+
+    private static final String TAG = RPrefs.class.getSimpleName();
 
     public static SharedPreferences prefs = ColorBlendr.getAppContext().createDeviceProtectedStorageContext().getSharedPreferences(Const.SharedPrefs, MODE_PRIVATE);
     static SharedPreferences.Editor editor = prefs.edit();
@@ -85,5 +97,44 @@ public class RPrefs {
 
     public static void clearAllPrefs() {
         editor.clear().apply();
+    }
+
+    public static void backupPrefs(final @NonNull OutputStream outputStream) {
+        try (outputStream; ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream)) {
+            objectOutputStream.writeObject(prefs.getAll());
+        } catch (IOException e) {
+            Log.e(TAG, "Error serializing preferences", e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void restorePrefs(final @NonNull InputStream inputStream) {
+        Map<String, Object> map;
+        try (inputStream; ObjectInputStream objectInputStream = new ObjectInputStream(inputStream)) {
+            map = (Map<String, Object>) objectInputStream.readObject();
+        } catch (ClassNotFoundException | IOException e) {
+            Log.e(TAG, "Error deserializing preferences", e);
+            return;
+        }
+
+        editor.clear();
+
+        for (Map.Entry<String, Object> e : map.entrySet()) {
+            if (e.getValue() instanceof Boolean) {
+                editor.putBoolean(e.getKey(), (Boolean) e.getValue());
+            } else if (e.getValue() instanceof String) {
+                editor.putString(e.getKey(), (String) e.getValue());
+            } else if (e.getValue() instanceof Integer) {
+                editor.putInt(e.getKey(), (int) e.getValue());
+            } else if (e.getValue() instanceof Float) {
+                editor.putFloat(e.getKey(), (float) e.getValue());
+            } else if (e.getValue() instanceof Long) {
+                editor.putLong(e.getKey(), (Long) e.getValue());
+            } else {
+                throw new IllegalArgumentException("Type " + e.getValue().getClass().getName() + " is unknown");
+            }
+        }
+
+        editor.apply();
     }
 }
