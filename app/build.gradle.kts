12d1ccc4e03b9807ad5c788b01006970ea83618e
DrDisagree <crazymahmud08@gmail.com>
2023-12-28 14:48:56 +0600

    Downgrade material dependency temporarily

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation("dev.rikka.shizuku:api:12.1.0")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
