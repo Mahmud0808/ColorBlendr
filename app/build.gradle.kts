f3469213fe71a681ca9b4ae22ad52e06a219ae18
DrDisagree <crazymahmud08@gmail.com>
2024-01-04 19:05:02 +0600

    Bump version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.drdisagree.colorblendr"
         minSdk = 31
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.0 beta 5"
+        versionCode = 6
+        versionName = "1.0 beta 6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
