5c55ef3b00c5169b6db2c3609a0c95deb08cb24d
DrDisagree <crazymahmud08@gmail.com>
2024-01-04 14:40:23 +0600

    Theme own app by default

--- app/src/main/java/com/drdisagree/colorblendr/utils/FabricatedUtil.java
+++ app/src/main/java/com/drdisagree/colorblendr/utils/FabricatedUtil.java
@@ -13,6 +13,7 @@ import android.graphics.Color;
 import androidx.annotation.ColorInt;
 import androidx.core.util.Pair;
 
+import com.drdisagree.colorblendr.BuildConfig;
 import com.drdisagree.colorblendr.common.Const;
 import com.drdisagree.colorblendr.config.RPrefs;
 import com.drdisagree.colorblendr.utils.fabricated.FabricatedOverlayResource;
@@ -124,6 +125,7 @@ public class FabricatedUtil {
         PackageManager packageManager = context.getPackageManager();
         List<ApplicationInfo> applications = packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
         HashMap<String, Boolean> selectedApps = new HashMap<>();
+        selectedApps.put(BuildConfig.APPLICATION_ID, true);
 
         for (ApplicationInfo appInfo : applications) {
             String packageName = appInfo.packageName;
