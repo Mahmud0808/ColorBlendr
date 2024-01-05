3f1eee3f4ce388c8700ef1c2fba8b050dcc578bd
DrDisagree <crazymahmud08@gmail.com>
2024-01-05 15:28:05 +0600

    Improve layout views

--- app/src/main/java/com/drdisagree/colorblendr/ui/widgets/MenuWidget.java
+++ app/src/main/java/com/drdisagree/colorblendr/ui/widgets/MenuWidget.java
@@ -5,9 +5,6 @@ import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.GradientDrawable;
-import android.os.Parcel;
-import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
@@ -15,50 +12,47 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import androidx.annotation.ColorInt;
 import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
 
 import com.drdisagree.colorblendr.R;
+import com.drdisagree.colorblendr.utils.ColorUtil;
 import com.drdisagree.colorblendr.utils.SystemUtil;
 import com.google.android.material.card.MaterialCardView;
 
-public class ColorPickerWidget extends RelativeLayout {
+public class MenuWidget extends RelativeLayout {
 
     private MaterialCardView container;
     private TextView titleTextView;
     private TextView summaryTextView;
     private ImageView iconImageView;
-    private View colorView;
-    private @ColorInt int selectedColor = Color.WHITE;
+    private ImageView endArrowImageView;
 
-    public ColorPickerWidget(Context context) {
+    public MenuWidget(Context context) {
         super(context);
         init(context, null);
     }
 
-    public ColorPickerWidget(Context context, AttributeSet attrs) {
+    public MenuWidget(Context context, AttributeSet attrs) {
         super(context, attrs);
         init(context, attrs);
     }
 
-    public ColorPickerWidget(Context context, AttributeSet attrs, int defStyleAttr) {
+    public MenuWidget(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init(context, attrs);
     }
 
     private void init(Context context, AttributeSet attrs) {
-        inflate(context, R.layout.view_widget_colorpicker, this);
+        inflate(context, R.layout.view_widget_menu, this);
 
         initializeId();
 
-        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ColorPickerWidget);
-        setTitle(typedArray.getString(R.styleable.ColorPickerWidget_titleText));
-        setSummary(typedArray.getString(R.styleable.ColorPickerWidget_summaryText));
-        int icon = typedArray.getResourceId(R.styleable.ColorPickerWidget_icon, 0);
-        boolean iconSpaceReserved = typedArray.getBoolean(R.styleable.SwitchWidget_iconSpaceReserved, false);
-        int colorResId = typedArray.getResourceId(R.styleable.ColorPickerWidget_previewColor, Integer.MIN_VALUE);
-        selectedColor = typedArray.getColor(R.styleable.ColorPickerWidget_previewColor, Color.WHITE);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.MenuWidget);
+        setTitle(typedArray.getString(R.styleable.MenuWidget_titleText));
+        setSummary(typedArray.getString(R.styleable.MenuWidget_summaryText));
+        int icon = typedArray.getResourceId(R.styleable.MenuWidget_icon, 0);
+        boolean iconSpaceReserved = typedArray.getBoolean(R.styleable.MenuWidget_iconSpaceReserved, false);
+        boolean showEndArrow = typedArray.getBoolean(R.styleable.MenuWidget_showEndArrow, false);
         typedArray.recycle();
 
         if (icon != 0) {
@@ -70,8 +64,8 @@ public class ColorPickerWidget extends RelativeLayout {
             iconImageView.setVisibility(GONE);
         }
 
-        if (colorResId != Integer.MIN_VALUE) {
-            setPreviewColor(ContextCompat.getColor(getContext(), colorResId));
+        if (showEndArrow) {
+            endArrowImageView.setVisibility(VISIBLE);
         }
     }
 
@@ -105,27 +99,18 @@ public class ColorPickerWidget extends RelativeLayout {
         iconImageView.setVisibility(visibility);
     }
 
-    public @ColorInt int getPreviewColor() {
-        return selectedColor;
+    public void setEndArrowVisibility(int visibility) {
+        endArrowImageView.setVisibility(visibility);
     }
 
-    public void setPreviewColor(@ColorInt int color) {
-        this.selectedColor = color;
+    @Override
+    public void setOnClickListener(@Nullable OnClickListener l) {
+        container.setOnClickListener(l);
+    }
 
-        if (!isEnabled()) {
-            if (SystemUtil.isDarkMode()) {
-                color = Color.DKGRAY;
-            } else {
-                color = Color.LTGRAY;
-            }
-        }
-
-        GradientDrawable drawable = new GradientDrawable(
-                GradientDrawable.Orientation.LEFT_RIGHT,
-                new int[]{color, color}
-        );
-        drawable.setShape(GradientDrawable.OVAL);
-        colorView.setBackground(drawable);
+    @Override
+    public void setOnLongClickListener(@Nullable OnLongClickListener l) {
+        container.setOnLongClickListener(l);
     }
 
     @Override
@@ -142,19 +127,23 @@ public class ColorPickerWidget extends RelativeLayout {
             a.recycle();
 
             iconImageView.setImageTintList(ColorStateList.valueOf(color));
+            endArrowImageView.setImageTintList(ColorStateList.valueOf(
+                    ColorUtil.getColorFromAttribute(getContext(), com.google.android.material.R.attr.colorOnSurface)
+            ));
         } else {
             if (SystemUtil.isDarkMode()) {
                 iconImageView.setImageTintList(ColorStateList.valueOf(Color.DKGRAY));
+                endArrowImageView.setImageTintList(ColorStateList.valueOf(Color.DKGRAY));
             } else {
                 iconImageView.setImageTintList(ColorStateList.valueOf(Color.LTGRAY));
+                endArrowImageView.setImageTintList(ColorStateList.valueOf(Color.LTGRAY));
             }
         }
 
         container.setEnabled(enabled);
+        iconImageView.setEnabled(enabled);
         titleTextView.setEnabled(enabled);
         summaryTextView.setEnabled(enabled);
-        iconImageView.setEnabled(enabled);
-        setPreviewColor(enabled ? getPreviewColor() : Color.GRAY);
     }
 
     // to avoid listener bug, we need to re-generate unique id for each view
@@ -163,73 +152,17 @@ public class ColorPickerWidget extends RelativeLayout {
         iconImageView = findViewById(R.id.icon);
         titleTextView = findViewById(R.id.title);
         summaryTextView = findViewById(R.id.summary);
-        colorView = findViewById(R.id.color_widget);
+        endArrowImageView = findViewById(R.id.end_arrow);
 
         container.setId(View.generateViewId());
         iconImageView.setId(View.generateViewId());
         titleTextView.setId(View.generateViewId());
         summaryTextView.setId(View.generateViewId());
-        colorView.setId(View.generateViewId());
+        endArrowImageView.setId(View.generateViewId());
 
         LayoutParams layoutParams = (LayoutParams) findViewById(R.id.text_container).getLayoutParams();
-        layoutParams.addRule(RelativeLayout.START_OF, colorView.getId());
+        layoutParams.addRule(RelativeLayout.START_OF, endArrowImageView.getId());
         layoutParams.addRule(RelativeLayout.END_OF, iconImageView.getId());
         findViewById(R.id.text_container).setLayoutParams(layoutParams);
-    }
-
-    @Override
-    public void setOnClickListener(@Nullable OnClickListener l) {
-        container.setOnClickListener(l);
-    }
-
-    @Override
-    protected Parcelable onSaveInstanceState() {
-        Parcelable superState = super.onSaveInstanceState();
-
-        SavedState ss = new SavedState(superState);
-        ss.selectedColor = selectedColor;
-
-        return ss;
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Parcelable state) {
-        if (!(state instanceof SavedState ss)) {
-            super.onRestoreInstanceState(state);
-            return;
-        }
-
-        super.onRestoreInstanceState(ss.getSuperState());
-
-        setPreviewColor(ss.selectedColor);
-    }
-
-    private static class SavedState extends BaseSavedState {
-        int selectedColor;
-
-        SavedState(Parcelable superState) {
-            super(superState);
-        }
-
-        private SavedState(Parcel in) {
-            super(in);
-            selectedColor = in.readInt();
-        }
-
-        @Override
-        public void writeToParcel(Parcel dest, int flags) {
-            super.writeToParcel(dest, flags);
-            dest.writeInt(selectedColor);
-        }
-
-        public static final Creator<SavedState> CREATOR = new Creator<>() {
-            public SavedState createFromParcel(Parcel in) {
-                return new SavedState(in);
-            }
-
-            public SavedState[] newArray(int size) {
-                return new SavedState[size];
-            }
-        };
     }
 }
