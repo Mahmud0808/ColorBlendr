9c7bc78a00d26db1271a0b7bd0c2af648e5ac052
DrDisagree <29881338+Mahmud0808@users.noreply.github.com>
2026-01-11 14:44:46 +0600

    Bump version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
     defaultConfig {
         minSdk = 31
         targetSdk = 36
-        versionCode = 38
-        versionName = "v2.0.3"
+        versionCode = 39
+        versionName = "v2.1"
 
         ndk {
             abiFilters.addAll(listOf("arm64-v8a", "armeabi-v7a", "x86", "x86_64"))
