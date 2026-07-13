ea66dbdd5062b6c086414fb56137a0a5a00f7d6e
DrDisagree <29881338+Mahmud0808@users.noreply.github.com>
2026-07-13 20:17:07 +0600

    Enhance WarningCard UI with rounded icon and improved click feedback

--- app/src/main/java/com/drdisagree/colorblendr/ui/compose/components/WarningCard.kt
+++ app/src/main/java/com/drdisagree/colorblendr/ui/compose/components/WarningCard.kt
@@ -1,15 +1,15 @@
 package com.drdisagree.colorblendr.ui.compose.components
 
 import androidx.compose.foundation.clickable
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Close
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -17,6 +17,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.dimensionResource
@@ -59,10 +61,12 @@ fun WarningCard(
                         tint = MaterialTheme.colorScheme.onPrimaryContainer,
                         modifier = Modifier
                             .size(24.dp)
+                            .clip(CircleShape)
                             .clickable {
                                 haptics.performHapticFeedback(HapticFeedbackType.ContextClick)
                                 onClose()
                             }
+                            .padding(4.dp)
                     )
                 }
             }
